7e4cec5c2fd50e697bd15445e3f992db4d116e2f
paulvee <pw.versteeg@gmail.com>
2020-10-16 21:13:28 +0200

    Add files via upload

---
+++ gps.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python2.7
+#-------------------------------------------------------------------------------
+# Name:        gps.py
+# Purpose:     Serial port monitoring of a ubloc NMEA
+#
+# Uses:        https://github.com/Knio/pynmea2
+# Author:      paulv
+#
+# Created:     20-07-2018
+# Copyright:   (c) paulv 2018 2019
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import serial
+import pynmea2
+from datetime import datetime, date, time, timedelta
+
+
+serialPort = serial.Serial("/dev/ttyAMA0", 9600, timeout=0.5)
+
+def parseGPS(str):
+
+    if str.find('GGA') > 0:
+        msg = pynmea2.parse(str)
+
+        # a kludge to get local time
+        local_time = ("{}:{}:{}".format(msg.timestamp.hour+2, msg.timestamp.minute, msg.timestamp.second))
+
+        print ("Time: {} \t Lat: {} {} \t Lon: {} {} \t Altitude: {} {} \t Satellites: {}"\
+            .format(local_time,msg.lat,msg.lat_dir,msg.lon,msg.lon_dir,msg.altitude,msg.altitude_units, msg.num_sats))
+
+
+def main():
+    print("starting processing")
+    while True:
+        try:
+            str = serialPort.readline()
+            parseGPS(str)
+        except pynmea2.ParseError as e:
+            print('Parse error: {}'.format(e))
+            continue
+        except serial.SerialException as e:
+            print('Device error: {}'.format(e))
+            break
+
+
+if __name__ == '__main__':
+    main()
+
