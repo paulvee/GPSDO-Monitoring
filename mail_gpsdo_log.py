ce3057c1d9e2abf7ddb50f97cb5426a19a5ad2da
paulvee <pw.versteeg@gmail.com>
2023-12-19 11:43:35 +0100

    Update mail_gpsdo_log.py

--- mail_gpsdo_log.py
+++ mail_gpsdo_log.py
@@ -25,7 +25,7 @@ DEBUG = False
 log_file = "/mnt/ramdisk/gpsdo.log"
 zip_file = "/mnt/ramdisk/gpsdo.zip"
 # target email account
-mail_address = "pw.versteeg@gmail.com"
+mail_address = "your e-mail address"
 
 
 def mail_err_log():
